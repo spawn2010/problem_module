3a91384c0704a83598aede1ef96284dabfe5ccef
user <spawn.2010@mail.ru>
2021-12-06 18:58:32 +0300

    add user_image migrations

---
+++ migrations/m211206_154009_user_image_field.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m211206_154009_user_image_field
+ */
+class m211206_154009_user_image_field extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('user', 'user_image', $this->binary());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('user','user_image');
+        return true;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m211206_154009_user_image_field cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
