d46565a4daf72b8aacb33b1c2b370c81908a60ee
user <spawn.2010@mail.ru>
2021-12-27 12:43:10 +0300

    corrected comments

--- views/problem/view.php
+++ views/problem/view.php
@@ -3,8 +3,7 @@
 /**
  * @var $problem
  */
-$datetime = new DateTimeImmutable($problem['created_at']);
-
+Yii::$app->timeZone = 'Europe/Moscow';
 ?>
 <div class="container mt-3 no_rating ">
     <div class="row border border-2">
@@ -15,7 +14,7 @@ $datetime = new DateTimeImmutable($problem['created_at']);
         <div class="w-100 p-1"></div>
         <div class="col text-left p-3">
                 <div class="border-top pt-3" id="problem"><h6>Автор: <?=$problem->user['username']?></h6></div>
-                <div><h6>Добавлено: <?=$datetime->format('d.m.Y в H:i')?></h6></div>
+                <div><h6>Добавлено: <?=Yii::$app->formatter->asDatetime($problem['created_at'], 'dd.MM.Y в HH:mm')?></h6></div>
         </div>
     </div>
 </div>
